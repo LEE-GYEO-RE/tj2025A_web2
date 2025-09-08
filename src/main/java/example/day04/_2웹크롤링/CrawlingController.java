bddd8831ce287adb2defea497789e6b9bf76ddd4
LEE-GYEO-RE <rufp9891@gmail.com>
2025-09-08 12:21:51 +0900

    web2_day04_크롤링

---
+++ src/main/java/example/day04/_2웹크롤링/CrawlingController.java
@@ -0,0 +1,33 @@
+package example.day04._2웹크롤링;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/task/day04")
+@RequiredArgsConstructor
+public class CrawlingController {
+
+
+    private final CrawlingService crawlingService;
+    // 1. 뉴스
+    @GetMapping("/craw1")
+    public List<String> task1(){
+        return crawlingService.task1();
+    }
+
+    // 2. 상품정보
+    @GetMapping("/craw2")
+    public List<Map<String , String >> task2(){
+        return crawlingService.task2();
+    }
+
+
+} // class e
---
+++ src/main/java/example/day04/_2웹크롤링/CrawlingService.java
@@ -0,0 +1,102 @@
+package example.day04._2웹크롤링;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class CrawlingService {
+
+    // 1. 뉴스 크롤링
+    public List<String> task1(){
+        List<String> list = new ArrayList<>();
+        try {
+
+            // 1-1 : 크롤링할 웹페이지 주소
+            String URL = "https://www.karnews.or.kr/news/articleList.html?sc_section_code=S1N1&view_type=sm";
+
+            // 1-2 : JSOUP 이용한 웹주소의 HTML(문서) 가져오기
+            // * Document import org.jsoup.nodes.Document;
+            // * Jsoup.connect( 크롤링할 주소 ).get() * 일반예외
+            Document document = Jsoup.connect(URL).get();
+
+            // 1-3 : ****************** 가져올 HTML 식별자 확인 .select("CSS선택자"); ******************
+            // JS : document.querySelect("CSS선택자");
+            // JSOUP : document.select("CSS선택자");
+            Elements aList = document.select(".titles > a "); // 'title' class 명을 가진 마크업 바로 아래 <a> 마크업
+
+            // 1-4 : 가져온 마크업들을 반복하여 텍스트만 추출
+            // .text() : <a> 마크업 사이 택스트 : 은마아파트, 재건축 최초 '역세권 특례' 적용…공공분양 신호탄
+            for (Element a : aList ){
+                String title = a.text();
+                if(title.isBlank()) continue; // 만일 내용이 없으면 다음반복
+                list.add(title);
+            } // for e
+
+        }catch (Exception e ){
+            System.out.println(e);
+        }
+        return list;
+    } // func e
+
+
+    // 2. 상품 정보 : https://www.yes24.com/robots.txt
+    public List<Map<String , String >> task2(){
+        // 2-1 : 책 정보들을 담을 리스트
+        List<Map<String , String >> list = new ArrayList<>();
+        try {
+            // 2-2 : 웹 크롤링할 주소
+            String URL = "https://www.yes24.com/product/category/daybestseller?categoryNumber=001&pageNumber=1&pageSize=24&type=day";
+            // 2-3 : JSOUP 활용한 지정한 주소 HTML로 가져오기
+            Document document = Jsoup.connect(URL).get();
+            // 2-4 : 책제목과 책가격의 css 선택자 확인 / 가져오기
+            // 책제목 : .info_name > gd_name
+            // 책가격 : .info_price > .txt.num > .yes_b
+            // 책이미지 : .img_bdr .lazy
+            Elements nameList = document.select(".info_name > .gd_name");
+            Elements priceList = document.select(".info_price .yes_b");
+            Elements imgList = document.select(".img_bdr .lazy");
+            // 2-5 : 반복문 이용한 책정보 구성
+            for( int i = 0; i < nameList.size() ; i++){
+                String name = nameList.get( i ).text(); // i번째 책제목 1개씩 호출
+                String price = priceList.get( i ).text(); // i번째 책가격 1개씩 호출
+                String img = imgList.get(i).attr("data-original"); // i번째 책이미지(링크) 속성값 1개 호출
+                Map< String  , String > map = new HashMap<>();
+                map.put("name" , name); // Map 객체 생성
+                map.put("price" , price);
+                map.put("img" , img);
+                list.add(map);
+            } // for e
+
+        }catch (Exception e ){
+            System.out.println(e);
+        }
+        return list;
+    } // class e
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+} // class e
