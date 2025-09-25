4b6583f3052f9ef6b922c539f655903170aba885
LEE-GYEO-RE <rufp9891@gmail.com>
2025-09-25 09:29:21 +0900

    web2_day12_결석 부문 (복습 필요)

--- src/main/reactapp/src/example/day05/실습7/components/Header.jsx
+++ src/main/reactapp/src/example/day05/실습7/components/Header.jsx
@@ -1,44 +1,36 @@
-import { useDispatch, useSelector } from "react-redux";
-import { BrowserRouter, Link, useNavigate } from "react-router-dom";
-import { login, logout } from "../store/userSlice";
+import { Link, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from 'react-redux'
+import { logout } from '../store/userSlice.jsx'
 
-export default function Header(props) {
+export default function Header( props ){
+    // [1] store 저장된 상태를 가져오기 , useSelector( (state)=>state.상태명 )
+    // const { isAuthenticated } = useSelector( (state)=> state.user );        console.log( isAuthenticated );
+    const { isAuthenticated , userInfo } = useSelector( ( state )=> state.user );
 
-    // const { isAuthenticated } = useSelector( (state) => state.user );
-    const { isAuthenticated , userInfo } = useSelector( (state) => state.user );
-
+    // [2] dispatch 이용한 상태 변경 하기 
     const dispatch = useDispatch();
-
-    const loginHandle = () =>{
-        dispatch( login() );
-    }
-    
     const navigate = useNavigate();
-
-    const logoutHandle = () => {
-        alert('로그아웃 되었습니다.');
-        dispatch( logout() ); // 상태 변경되면 **리렌더링**
-        navigate('/')
+    const onLogout = async()=>{
+        alert('[로그아웃 성공]');
+        dispatch( logout() ); // 상태가 변경되면 **리렌더링** 된다.
+        navigate('/login')
     }
 
-    return (<>
-        {isAuthenticated
-            ?
-            <div>
-                <ul>
-                    <li> <span> 안녕하세요 { userInfo.name } 님 </span></li>
-                    <li> <Link to="/"> 메인페이지 </Link></li>
-                    <li> <Link to="/profile"> 프로필페이지 </Link></li>
-                    <button onClick={logoutHandle} > 로그아웃 </button>
-                </ul>
-            </div>
-            :
-            <div>
-                <ul>
-                    <li> <Link to="/"> 메인페이지 </Link></li>
-                    <li> <Link to="/login"> 로그인 페이지 </Link></li>
-                </ul>
-            </div>
-        }
+    // 서버에게 로그인 상태 요청 useEffect( ()=>{} , [] ) 
+
+    return(<>
+        <h3> 헤더 </h3>
+        <ul>
+            <li> <Link to ="/"> 홈 </Link></li>
+            { isAuthenticated==true ? 
+                <> 
+                    <li> <span> 안녕하세요. { userInfo.name } 님 </span></li>
+                    <li> <Link to ="/profile"> 프로필 </Link></li>
+                    <li> <button onClick={ onLogout }> 로그아웃 </button></li>
+                </> 
+                : 
+                <>  <li> <Link to ="/login"> 로그인 </Link></li> </> 
+            }
+        </ul>
     </>)
 }
--- src/main/reactapp/src/example/day05/실습7/store/userSlice.jsx
+++ src/main/reactapp/src/example/day05/실습7/store/userSlice.jsx
@@ -1,36 +1,29 @@
 /*
-    슬라이스 : 상태(state) , 리듀서(reducer) , 액션(action) 정의하는 곳
+    슬라이스 : 상태(state), 리듀서(reducer), 액션(action) 정의하는 곳 
     createSlice
-
 */
+import { createSlice } from '@reduxjs/toolkit'
 
-import { createSlice } from "@reduxjs/toolkit"
-
-// [1] 상태의 초기값 정의 , 로그인 여부 , 로그인한 회원 정보
+//[1] 상태의 초기값 정의 , 로그인여부 , 로그인회원정보
 const initialState = { isAuthenticated : false , userInfo : null }
-
-// [2] 슬라이스 정의
-const userSlice = createSlice( {
-    name : 'user', // 슬라이스(상태) 명 
-    initialState , 
-    reducers : { // 여러개의 상태변경함수를 정의한 곳 { key : value } 형식
-        login : ( state , action ) => { 
-            state.isAuthenticated = true; 
-            // action 할때 전달되는 매개변수들이 payload 안에 값에 들어있음.
-            // 예) dispatch( login("안녕") ) , payload = "안녕" 
-            state.userInfo = action.payload; 
+//[2] 슬라이스 정의
+const userSlice = createSlice({
+    name : "user" , // 슬라이스(상태) 명
+    initialState , // 상태의 초기값 
+    reducers : { // 여러개의 상태 변경 함수 정의한곳, { 함수명 : (state)=>{} , 함수명 : (state)=>{} }
+        login : ( state , action )=>{ 
+            state.isAuthenticated = true; // 로그인 여부 true 로 변경 
+            // action 할때 전달되는 매개변수들을 payload 안에 값이 들어있다.
+             // 예] dispatch( login( "안녕" ) ) , payload = "안녕"
+            state.userInfo = action.payload;
         },
         logout : ( state ) => { 
-            state.isAuthenticated = false; // 로그인 여부 false로 변경 
-            state.userInfo = null;  // 로그인회원정보 null로 변경
+            state.isAuthenticated = false; // 로그인 여부 false 로 변경 
+            state.userInfo = null; // 로그인회원정보 null 로 변경 
         }
-    }
-
-});
-
-// [3] store 에 저장할 수 있게 export 해주기
-export default userSlice.reducer // 현재 정의한 reducers 를 store에 등록 예정
-
-// [4] 다른 컴포넌트에서 액션이 가능하도록 login , logout export 해주기
+    }  
+})
+// [3] store 에 저장할수 있게 export 해주기 
+export default userSlice.reducer // 현재 정의한 리듀서(reducers)들을 store 등록 예정 
+// [4] 다른 컴포넌트에서 액션이 가능하도록 login,logout export해주기
 export const { login , logout } = userSlice.actions
-
