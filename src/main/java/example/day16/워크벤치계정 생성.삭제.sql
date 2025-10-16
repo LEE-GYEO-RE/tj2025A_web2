275949c595f5f842a029450d033747d29a08c559
LEE-GYEO-RE <rufp9891@gmail.com>
2025-10-16 14:20:34 +0900

    web2_day16_워크벤치 계정 생성, 인덱스 생성, 조회

---
+++ src/main/java/example/day16/워크벤치계정 생성.삭제.sql
@@ -0,0 +1,57 @@
+-- DCL : 계정 관리 , 계정의 사용권한 관리
+# [1] 계정 만들기 : root(최고관리자) 계정으로 가능
+# 데이터베이스 설치하면 기본적으로 root 계정이 존재 < 강의기준 : root 사용중 >
+
+# create user '계정명'@'허용도메인' identified by '비밀번호'
+create user 'dev1'@'localhost' identified by '1234';	-- 로컬 전용
+create user 'dev2'@'%' identified by 'abcd';			-- 외부 접속 모두 허용
+
+# [2] (워크벤치) 다른 계정으로 접속하기 : 
+# 1. 워크벤치 홈 메뉴에서 ( Database -> Connect to database ) --
+# 2. username : 계정명 , password : 비밀번호 , [ok] 
+# 다른 계정으로 접속 후 : show databases;
+
+# [3] 계정의 권한 부여 ( GRANT ) , all privileges : springweb2에 대한 모든 권한 허용
+# grant all privileges on 데이터베이스명.테이블/뷰명 to "계정명"@"도메인";
+grant all privileges on springweb2.* to "dev1"@"localhost"; # 확인 : dev1 계정에서 접속후 확인
+grant select on springweb2.student to 'dev2'@'%';			
+# 특정한 테이블만 조회 권한 부여  ( student 테이블에서 select 조회만)
+-- use springweb2; 			됨
+-- select * from student;	됨
+-- delete from student;		안됨
+
+# 계정 권한부여 확인
+# show grants for "계정명"@"도메인";
+show grants for "dev1"@"localhost";
+
+# [4] 계정의 권한 취소/회수 ( REVOKE )
+# revoke select on 데이터베이스명.테이블/뷰명 from '계정명'@'도메인';
+revoke select on springweb2.student from 'dev2'@'%';
+# ==> schema 없어져있음
+
+# [5] 계정의 비밀번호 변경 
+# alter user '계정명'@'도메인' identified by '새로운비밀번호';
+alter user 'dev2'@'%' identified by '1234';
+# 인텔리제이 ,  VSCODE = IDE 통합개발 환경 : 코드를 편하게 사용할 수 있게 도와주는 소프트웨어
+# 워크벤치 : DB를 편하게 사용할 수 있게 도와주는 소프트웨어
+
+# [6] 계정 삭제
+# drop user '계정명'@'도메인';
+drop user 'dev2'@'%';
+
+# 1. 새로운 계정 생성
+create user 'dev3'@'localhost' identified by '1234';
+
+# 2. 새로운 뷰 생성
+use springweb2;
+create or replace view student_view as select * from student;
+
+# 3. 새로운 계정에게 뷰 조회 권한 만 부여
+grant select on springweb2.student_view to 'dev3'@'localhost';
+
+# 4. (dev) dev3 접속후 테스트
+select * from student_view; -- 가능
+update student_view set name = '유재석'; -- 불가능
+
+# 데이터베이스 내 모든 계정 목록 확인
+select * from mysql.user;
